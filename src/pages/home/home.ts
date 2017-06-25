4cbeea5d3c785b0045f3043a5894055f904bdb54
santosh <santosh.shinde@sixfactors.ca>
2017-06-25 19:54:49 +0530

    setup karma and jasmin

---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title: string = "Home";
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  changeTitle(title){
+          this.title = title;
+  }
+
+}
